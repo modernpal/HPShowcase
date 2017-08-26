19fae5f00eaeaf0d461832322de5fd56339d2523
Arash Farahani <a.farahani@modernpal.com>
2017-08-26 11:30:50 +0430

    Initial Commit

---
+++ HPShowCase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HPShowCase
+//
+//  Created by Arash Farahani on 8/26/17.
+//  Copyright © 2017 Arash Farahani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
